dac7a010ec7abe5adf53aea527356b166b48b4ae
wojtaszek171 <pawelwojtaszko@o2.pl>
2018-01-31 22:23:28 +0100

    Started project. Connected app with Parse platform. Added User Sign up and Log in

---
+++ app/src/main/java/com/example/pawel/communicator/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.pawel.communicator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button login = findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String pseudonym = ((EditText) findViewById(R.id.pseudonym)).getText().toString();
+                final String password = ((EditText) findViewById(R.id.pass)).getText().toString();
+                if (pseudonym != null && password != null) {
+                                    ParseUser.logInInBackground(pseudonym, password, new LogInCallback() {
+                                        @Override
+                                        public void done(ParseUser user, ParseException e) {
+                                            if (user != null) {
+                                                // Hooray! The user is logged in.
+                                                Toast.makeText(LoginActivity.this, "Pomyślnie zalogowano", Toast.LENGTH_SHORT).show();
+                                                finish();
+                                                startActivity(new Intent(getApplicationContext(), FriendsActivity.class));
+                                            } else {
+                                                // Signup failed. Look at the ParseException to see what happened.
+                                                // Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                switch (e.getMessage()){
+                                                    case "password is required.":
+                                                        Toast.makeText(getApplicationContext(), "Hasło jest wymagane", Toast.LENGTH_SHORT).show();
+                                                        break;
+                                                    case "Invalid username/password.":
+                                                        Toast.makeText(getApplicationContext(), "Błędna nazwa użytkownika/hasło", Toast.LENGTH_SHORT).show();
+
+                                                }
+
+                                            }
+                                        }
+                                    });
+
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/pawel/communicator/RegisterActivity.java
@@ -0,0 +1,51 @@
+package com.example.pawel.communicator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Button register = findViewById(R.id.register_button);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText) findViewById(R.id.email)).getText().toString();
+                String pseudonym = ((EditText) findViewById(R.id.pseudonym)).getText().toString();
+                String password = ((EditText) findViewById(R.id.pass)).getText().toString();
+                String password_2 = ((EditText) findViewById(R.id.pass_repeat)).getText().toString();
+                if(email != null && pseudonym != null && password !=null && password_2 !=null){
+                    ParseUser user = new ParseUser();
+                    user.setUsername(pseudonym);
+                    user.setEmail(email);
+                    user.setPassword(password);
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Intent intent = new Intent(getApplicationContext(), FriendsActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(getApplicationContext(), "Zarejestrowano pomyślnie!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Coś poszło nie tak...", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
