727dad06cba92d992375493ea7f8b382ccbb343f
wojtaszek171 <pawelwojtaszko@o2.pl>
2018-02-01 00:20:56 +0100

    simple bottom menu after login

--- app/src/main/java/com/example/pawel/communicator/FriendsActivity.java
+++ app/src/main/java/com/example/pawel/communicator/FriendsActivity.java
@@ -1,7 +1,13 @@
 package com.example.pawel.communicator;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        BottomNavigationView bottomMenu =(BottomNavigationView) findViewById(R.id.menuBottom);
+        bottomMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_friends:
+                        Toast.makeText(getApplicationContext(), "Znajomi", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_conversations:
+                        Toast.makeText(getApplicationContext(), "Rozmowy", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
